31df296004199ebf7c94f81f73d30486a94d6bcc
Gaston Gouveia <gastongouveia@gmail.com>
2020-10-08 17:30:49 -0300

    adicionado rota

--- app/routes/indexRT.js
+++ app/routes/indexRT.js
@@ -5,5 +5,9 @@ module.exports = function(mocai){
   mocai.get('/', function (req, res) {//ROTA INICIAL DA APLICAÇÃO
     res.render('index');  
   });
+
+  mocai.get('/contatos', function (req, res) {//ROTA INICIAL DA APLICAÇÃO
+    res.render('contatos');  
+  });
   
 };
