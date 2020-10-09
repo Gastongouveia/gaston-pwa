896edaa7f75c4f0cdaf382d6cae22da71a4a14c9
Gaston Gouveia <gastongouveia@gmail.com>
2020-10-09 16:24:59 -0300

    for

--- app/routes/indexRT.js
+++ app/routes/indexRT.js
@@ -3,7 +3,20 @@
 module.exports = function(mocai){
 
   mocai.get('/', function (req, res) {//ROTA INICIAL DA APLICAÇÃO
-    res.render('index');  
+    var dados = {
+      'pessoa': [{
+        'nome':'Gaston',
+        'idade':30,
+        'peso':100,
+        'profissao':'Estudante'
+      },{
+        'nome':'Henning',
+        'idade':37,
+        'peso':90,
+        'profissao':'Professor'
+      }]
+    }
+    res.render('index', {dados});  
   });
 
   mocai.get('/contatos', function (req, res) {//ROTA INICIAL DA APLICAÇÃO
